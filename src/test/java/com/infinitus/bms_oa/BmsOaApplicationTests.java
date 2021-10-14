3a98a542de8a1d7868567234145a09cdf215eb21
bms_oa <944714386@qq.com>
2021-10-14 17:03:59 +0800

    1.0版本

---
+++ src/test/java/com/infinitus/bms_oa/BmsOaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitus.bms_oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsOaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
