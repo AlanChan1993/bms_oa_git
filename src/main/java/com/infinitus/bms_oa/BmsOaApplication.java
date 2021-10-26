08e3a6b5e65de6874253f3127e9ee4ba8bd4fa99
bms_oa <944714386@qq.com>
2021-10-26 09:01:30 +0800

    2.0版本

--- src/main/java/com/infinitus/bms_oa/BmsOaApplication.java
+++ src/main/java/com/infinitus/bms_oa/BmsOaApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.infinitus.bms_oa.mapper")
 @EnableScheduling
+//@EnableTransactionManagement//用于事务回滚
 public class BmsOaApplication {
     public static void main(String[] args) {
         SpringApplication.run(BmsOaApplication.class, args);
--- src/main/java/com/infinitus/bms_oa/controller/UserController.java
+++ src/main/java/com/infinitus/bms_oa/controller/UserController.java
@@ -9,6 +9,7 @@ import com.infinitus.bms_oa.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +52,7 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping("hello")
+    //@Transactional
     public Infinitus hello(Integer id) {
         try {
             infinitus = infinitusService.getInfinitusById(id);
