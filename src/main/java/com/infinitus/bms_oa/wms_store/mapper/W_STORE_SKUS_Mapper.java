f542df164a23d64ef84a07e41af1beba970f4955
EDZ <944714386@qq.com>
2022-07-13 09:46:06 +0800

    4.0

--- src/main/java/com/infinitus/bms_oa/wms_store/mapper/W_STORE_SKUS_Mapper.java
+++ src/main/java/com/infinitus/bms_oa/wms_store/mapper/W_STORE_SKUS_Mapper.java
@@ -2,6 +2,7 @@ package com.infinitus.bms_oa.wms_store.mapper;
 
 import com.infinitus.bms_oa.wms_store.empty.W_STORE_SKUS;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.mybatis.spring.annotation.MapperScan;
 
 import java.util.List;
@@ -10,7 +11,9 @@ import java.util.List;
 public interface W_STORE_SKUS_Mapper {
     boolean insertW_STORE_SKUS(W_STORE_SKUS w_store_skus);
 
+    List<W_STORE_SKUS> queryW_STORE_SKUS();
+
     boolean deleteW_STORE_SKUS();
 
-    List<W_STORE_SKUS> queryW_STORE_SKUS();
+    boolean insertSkusList(@Param("list")List<W_STORE_SKUS> list);
 }
