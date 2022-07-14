b261fc4bef49980cb71f1b1a6f496c517a29af15
EDZ <944714386@qq.com>
2022-07-14 13:43:45 +0800

    skus的第一个完成版本

--- src/main/java/com/infinitus/bms_oa/wms_store/service/W_STORE_COMMODITIES_Service.java
+++ src/main/java/com/infinitus/bms_oa/wms_store/service/W_STORE_COMMODITIES_Service.java
@@ -8,4 +8,8 @@ public interface W_STORE_COMMODITIES_Service {
     boolean updateW_STORE_COMMODITIES(W_STORE_COMMODITIES w_store_commodities);
 
     List<W_STORE_COMMODITIES> queryW_STORE_COMMODITIES();
+
+    boolean deleteW_STORE_COMMODITIES();
+
+    boolean insertW_STORE_COMMODITIESList(List<W_STORE_COMMODITIES> commodities);
 }
--- src/main/java/com/infinitus/bms_oa/wms_store/task/WmsStoreTasks.java
+++ src/main/java/com/infinitus/bms_oa/wms_store/task/WmsStoreTasks.java
@@ -3,7 +3,9 @@ package com.infinitus.bms_oa.wms_store.task;
 import com.alibaba.fastjson.JSONObject;
 import com.infinitus.bms_oa.utils.HttpUtil;
 import com.infinitus.bms_oa.wms_store.empty.PagePojo;
+import com.infinitus.bms_oa.wms_store.empty.W_STORE_COMMODITIES;
 import com.infinitus.bms_oa.wms_store.empty.W_STORE_SKUS;
+import com.infinitus.bms_oa.wms_store.mapper.W_STORE_COMMODITIES_Mapper;
 import com.infinitus.bms_oa.wms_store.service.W_STORE_SKUS_Service;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.HmacAlgorithms;
@@ -41,24 +43,34 @@ public class WmsStoreTasks {
     private String listskus;
 
     @Value("${wms.ListCommodities.value}")
-    private String ListCommodities;
+    private String listCommodities;
 
     @Autowired
     private W_STORE_SKUS_Service store_skus_service;
 
+    @Autowired
+    private W_STORE_COMMODITIES_Mapper commoditiesMapper;
+
 
     private static final DateTimeFormatter RFC_7231_FORMATTER = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss O").withLocale(Locale.ENGLISH);
+
     /**
      * 定时任务 生产一小时执行一次
-     *
-     * */
+     */
     @Scheduled(fixedRate = 1000 * 60 * 60)
-    public void getSkuTask() throws UnsupportedEncodingException {
+    public void getStoreDetailTask() throws UnsupportedEncodingException {
+        getSkus();
+        getCommodities();
+    }
+
+    /**
+     * 批量获取商品中心 skus表
+     * */
+    public void  getSkus() throws UnsupportedEncodingException {
         //1、商品中心给出的demo中的加密与打印请求头
         String method = "GET";
         String uri = "/product-api/skus";
-        // 时间
-        String date = RFC_7231_FORMATTER.format(ZonedDateTime.now(UTC));
+        String date = RFC_7231_FORMATTER.format(ZonedDateTime.now(UTC));// 时间
         // 时间 + 请求类型 + 请求uri 的加密
         String signature =encodeBase64String(new HmacUtils(HmacAlgorithms.HMAC_SHA_256, secret).hmac("date: " + date + "\n" + method + " " + uri + " HTTP/1.1"));
         // HMAC 授权
@@ -99,5 +111,54 @@ public class WmsStoreTasks {
         }
 
     }
+
+    /**
+     * 批量获取商品中心Commodities的接口数据
+     */
+    public void getCommodities() throws UnsupportedEncodingException {
+//1、商品中心给出的demo中的加密与打印请求头
+        String method = "GET";
+        String uri = "/product-api/commodities";
+        String date = RFC_7231_FORMATTER.format(ZonedDateTime.now(UTC));// 时间
+        // 时间 + 请求类型 + 请求uri 的加密
+        String signature =encodeBase64String(new HmacUtils(HmacAlgorithms.HMAC_SHA_256, secret).hmac("date: " + date + "\n" + method + " " + uri + " HTTP/1.1"));
+        // HMAC 授权
+        String authorization = "hmac username=\"" + keyId + "\", algorithm=\"hmac-sha256\", headers=\"date request-line\", signature=\"" + signature + "\"";
+        // 打印请求头
+        //log.info("Authorization: {}" , authorization);
+        //log.info("Date: {}", date);
+        //2、拼接第一次请求的url并请求接口
+        String urlAll = url + listCommodities+"nos=&page=0&size=200";
+        String result = HttpUtil.httpGet(urlAll, authorization, date);
+        JSONObject jsonResult = JSONObject.parseObject(result);
+        log.info("jsonResult=:{}", jsonResult);
+
+        //3、将第一页获取的200条数据封装 并批量插入数据
+        List<W_STORE_COMMODITIES> commoditiesList = (List<W_STORE_COMMODITIES>) jsonResult.get("commodities");
+        //批量插入
+        if(commoditiesList!=null){
+            commoditiesMapper.deleteW_STORE_COMMODITIES();//批量更新前先清理掉之前的数据
+            commoditiesMapper.insertW_STORE_COMMODITIESList(commoditiesList);
+            log.info("commoditiesList插入成功=:{}", commoditiesList);
+        }
+
+        //4、获取总的页数，进行分页获取计算，每次获取后批量插入200条
+        PagePojo pagePojo = JSONObject.toJavaObject(JSONObject.parseObject(JSONObject.toJSONString(jsonResult.get("page"))),PagePojo.class);
+        if (pagePojo == null) {
+            return ;
+        }
+        for (int i = 1; i < pagePojo.getTotalPages(); i++) {
+            urlAll = url + listCommodities + "nos=&page=" + i + "&size=200";
+            String result2 = HttpUtil.httpGet(urlAll, authorization, date);
+            JSONObject jsonResult2 = JSONObject.parseObject(result2);
+            //log.info("i=:{}", i);//用于检查循环数
+            List<W_STORE_COMMODITIES> commoditiesList2 = (List<W_STORE_COMMODITIES>) jsonResult2.get("commodities");
+            if(commoditiesList2!=null) {
+                commoditiesMapper.insertW_STORE_COMMODITIESList(commoditiesList2);
+                log.info("commoditiesList" + i + "插入成功=:{}", commoditiesList2);
+            }
+        }
+    }
+
 
 }
